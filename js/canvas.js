2223f2f23ad2e9065766b5c663a44d1e429aec8e
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-07-10 23:04:57 +0900

    fill mode 추가
    *modeValue if문 활용하여 Text변경
    
    * canvas.addEventListener("click", onFillng); 실행시 modeValue 값을 확인하여 ctx.fillRect(0, 0, 700, 700); 실행시켜 canvas 전체에 color를 입힘

--- js/canvas.js
+++ js/canvas.js
@@ -2,14 +2,16 @@ const canvas = document.getElementById("js-canvas");
 const colors = document.getElementsByClassName("js-color");
 const ctx = canvas.getContext("2d");
 const range = document.getElementById("js-range");
-
+const modeBtn = document.getElementById("js-mode");
 canvas.width = 700;
 canvas.height = 700;
 
 ctx.strokeStyle = "#2c2c2c";
+ctx.fillStyle = "";
 ctx.lineWidth = 2.5;
 
 let mouseValue = false;
+let modeValue = false;
 
 function onMouseDown() {
   mouseValue = true;
@@ -31,10 +33,25 @@ function onMouseMove(ev) {
 function chageColor(ev) {
   strokeColor = ev.target.style.backgroundColor;
   ctx.strokeStyle = strokeColor;
+  ctx.fillStyle = strokeColor;
 }
 function hendleRangeWidth(ev) {
   const RangeWidth = ev.target.value;
   ctx.lineWidth = RangeWidth;
+}
+function hendlerMode() {
+  if (modeValue) {
+    modeValue = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    modeValue = true;
+    modeBtn.innerText = "Paint";
+  }
+}
+function onFillng() {
+  if (modeValue === true) {
+    ctx.fillRect(0, 0, 700, 700);
+  }
 }
 
 if (canvas) {
@@ -42,10 +59,14 @@ if (canvas) {
   canvas.addEventListener("mousedown", onMouseDown);
   canvas.addEventListener("mouseup", onMouseUp);
   canvas.addEventListener("mouseout", onMouseUp);
+  canvas.addEventListener("click", onFillng);
 }
 Array.from(colors).forEach(function (color) {
   color.addEventListener("click", chageColor);
 });
 if (range) {
   range.addEventListener("input", hendleRangeWidth);
+}
+if (modeBtn) {
+  modeBtn.addEventListener("click", hendlerMode);
 }
